a29d606397e213f86394d4132019173d1f5c7f6e
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-12-08 16:04:09 -0300

    Tipos Primitivos - Parte 1

---
+++ Primeiros passos/Tipos_Primitivos.js
@@ -0,0 +1,15 @@
+// Tipo: Number
+let num_01 = 10;
+let num_02 = 50;
+let num_03 = -1;
+let num_04 = -0.333;
+
+// Tipo: String
+let string_01 = "JavaScript";
+let string_02 = 'JavaScript';
+let string_03 = `JavaScript`;
+
+
+// Tipo: Booleano
+console.log(num_01 == Number)
+console.log(num_01 == String)
