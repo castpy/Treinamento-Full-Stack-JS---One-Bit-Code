3db5e8619b937e81e97eb1862474ecb8973c6a4e
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-12-10 16:42:24 -0300

    Operações Básicas - Parte 1

---
+++ Primeiros passos/Operações_Basicas.js
@@ -0,0 +1,63 @@
+// Adição
+let ad1 = 1
+let ad2 = 4
+console.log(ad1 + ad2)
+
+let ad3 = 5
+let ad4 = -7
+console.log(ad3 + ad4)
+
+// Subtração
+let sub1 = 10
+let sub2 = 3
+console.log(sub1 - sub2)
+
+let sub3 = 4.5
+let sub4 = 0.3
+console.log(sub3 - sub4)
+
+// Multiplicação
+let mul1 = 5
+let mul2 = 9
+console.log(mul1 * mul2)
+
+let mul3 = 7.1
+let mul4 = -5
+console.log(mul3 * mul4)
+
+// Divisão
+let div1 = 6
+let div2 = 2
+console.log(div1 / div2)
+
+let div3 = 8.5
+let div4 = 5
+console.log(div3 / div4)
+
+// Módulo
+let mod1 = 10
+let mod2 = 2
+console.log(mod1 % mod2)
+
+let mod3 = 15
+let mod4 = 3
+console.log(mod3 % mod4)
+
+// Concatenação
+let con1 = "Planeta"
+let con2 = "Terra"
+console.log(con1 + con2)
+console.log("Planeta" + "Terra")
+
+let con3 = "Planeta "
+let con4 = "Terra"
+console.log(con3 + con4)
+console.log("Planeta " + "Terra")
+
+// Extração
+let ext1 = "Marte"
+console.log(ext1[2])
+
+// Tamanho
+let tam1 = "Planeta Terra"
+console.log(tam1.length)
